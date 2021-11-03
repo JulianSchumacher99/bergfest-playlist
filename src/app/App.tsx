12017ddfa86bd51599242f4be0442308d5a87de5
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-11-03 11:38:23 +0100

    ✨Added and designed the first page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import styles from './App.module.css';
 
 function App(): JSX.Element {
-  return <main>Hello World</main>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.container__content}>
+        <h1 className={styles.title}>Bergfest</h1>
+        <form className={styles.form}>
+          <div className={styles.input__text}>
+            <span>Vorname</span>
+            <input
+              className={styles.input__field}
+              type="text"
+              id="FirstName"
+              name="Vorname"
+            ></input>
+          </div>
+          <div className={styles.input__text}>
+            <span>Nachname</span>
+            <input
+              className={styles.input__field}
+              type="text"
+              id="LastName"
+              name="Nachname"
+            ></input>
+          </div>
+          <input
+            className={styles.button}
+            type="submit"
+            id="Submit"
+            value="Los geht's"
+          ></input>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default App;
